9a00de617948b2d4012de1c7a1ef217528540d81
Rutwik <rutwikpatel08@gmail.com>
2026-05-22 14:47:06 +0530

    remove key

--- src/socket.ts
+++ src/socket.ts
@@ -73,6 +73,7 @@ export const initSocket = async (server: HttpServer, chessService: ChessService)
       origin: process.env.ORIGINS?.split(",") || "*",
       credentials: true,
     },
+    transports: ["websocket"]
   });
 
   io.use(socketAuthAdapter);
@@ -80,7 +81,6 @@ export const initSocket = async (server: HttpServer, chessService: ChessService)
   const pubClient = new createClient({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT || 6379),
-    db: 12,
     lazyConnect: true,
     maxRetriesPerRequest: null,
     enableReadyCheck: true,
@@ -90,7 +90,21 @@ export const initSocket = async (server: HttpServer, chessService: ChessService)
   await pubClient.connect();
   await subClient.connect();
 
-  io.adapter(createAdapter(pubClient, subClient));
+  const pong = await pubClient.ping();
+
+  logger.info("Redis pubClient ping:", pong);
+
+  io.adapter(createAdapter(pubClient, subClient, {
+    key: "backend_socket.io-redis-adapter",
+  }));
+
+  const testClient = pubClient.duplicate();
+  await testClient.connect();
+  testClient.subscribe("backend_socket.io-redis-adapter#test", (message, channel) => {
+    logger.info("Sub received:", channel, message);
+  });
+  pubClient.publish("backend_socket.io-redis-adapter#test", "hello");
+
   logger.info("Redis adapter initialized for Socket.IO");
 
   io.on("connection", (socket: Socket) => {
@@ -99,7 +113,7 @@ export const initSocket = async (server: HttpServer, chessService: ChessService)
     const playerId = user?._id?.toString() || guest?._id?.toString();
 
     logger.info(`Socket connected: ${socket.id}`, user ? `User:${user._id}` : `Guest:${guest?._id}`);
-
+    logger.info(`Socket connected: ${socket.id} on PID: ${process.pid}`);
     // ── Join a game ────────────────────────────────────────────────────────
     socket.on("join_game", async (_game: { gameId: string }) => {
       try {
